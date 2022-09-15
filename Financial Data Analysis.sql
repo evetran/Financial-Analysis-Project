abf339210ec963dca10d9cfb8479c084579e2a86
evetran <td.huyen0212@gmail.com>
2022-09-15 16:43:11 -0400

    initial commit

---
+++ Financial Data Analysis.sql
@@ -0,0 +1,164 @@
+-- Based on the database I created, I am going to use SQL to answer some questions about financial analysis
+
+
+-- LOOKING AT THE ^DJI --
+
+-- Calculate number of tickers in ^DJI index
+SELECT COUNT(DISTINCT Symbol) FROM DJIA;
+
+
+-- The ^DJI comprises 30 blue-chip stocks that are tops in their industries. But what are the TOP 3 Dow Jones stocks that are better buys than others?
+-- Looking at the TOP 3 stocks of ^DJI that have the most days in which close prices higher than open prices in 2020-2022
+SELECT Symbol, COUNT(*) AS days
+FROM DJIA d 
+WHERE ("Close" > "Open") AND STRFTIME('%Y', Date) BETWEEN '2020' AND '2022' 
+GROUP BY Symbol
+ORDER BY days DESC 
+LIMIT 3;
+
+
+
+-- Looking at the change of close price with close price of the date before
+SELECT 
+	Symbol, Date, Close,
+	ROUND((Close - previous_close), 2) AS 'Change',
+	ROUND(((Close - previous_close)*100/previous_close),2) AS '%Change',
+	CASE 
+		WHEN Close > previous_close THEN 'Up'
+		WHEN Close < previous_close THEN 'Down'
+	END AS 'Indicator'
+FROM
+	(SELECT 
+		Symbol, DATE(date) AS Date, ROUND(Close, 2) AS Close, 
+		LAG (Close, 1, 0) OVER (PARTITION  BY Symbol ORDER BY Date) AS previous_close
+	FROM DJIA d 
+	WHERE STRFTIME('%Y', date) BETWEEN '2020' AND '2022'
+	ORDER BY Symbol, date DESC)
+	
+
+--Looking at the total yearly trading volume and fundamental information of ^DJI stocks
+SELECT 
+	d1.Symbol,
+	d2.sector,
+	d1.Yearly_Aggregrate_Volume,
+	d2.totalRevenue,
+	d2.revenueGrowth,
+	d2.returnOnEquity 
+FROM
+	(SELECT Symbol, SUM(Volume) AS Yearly_Aggregrate_Volume FROM DJIA GROUP BY Symbol) d1
+LEFT JOIN DOWFundamental d2 
+ON d1.Symbol = d2.symbol
+
+	
+	
+	
+-- LOOKING AT APPLE STOCK --
+	
+-- Calculate number of days that close price of AAPL is higher than open price
+SELECT COUNT(*) 
+FROM DJIA d 
+WHERE 
+	("Close" > "Open") AND 
+	(Symbol = 'AAPL') AND 
+	(STRFTIME('%Y', Date) = '2022');
+
+
+-- Calculate the 50 days moving average of Apple stock
+SELECT 
+	DATE(Date), 
+	Close,
+	AVG(Close) OVER (PARTITION BY Symbol ORDER BY Date ROWS 50 PRECEDING) AS MA50
+FROM DJIA
+WHERE Symbol = 'AAPL' AND (STRFTIME('%Y', Date) BETWEEN '2000' AND '2022')
+ORDER BY Date DESC;
+
+
+
+-- Compare the 50 days moving average with 100 days moving average of APPL from 2020-2022, 
+-- we can judge whether the stock is bullish if MA50 > MA100, or bearish by versa
+SELECT 
+	DATE(Date) AS Date, ROUND(Close,2), ROUND(MA50, 2) AS MA50, ROUND(MA100, 2) AS MA100,
+	CASE 
+		WHEN MA50 > MA100 THEN 'Bullish'
+		WHEN MA50 < MA100 THEN 'Bearish'
+	END AS 'Indicator'
+FROM
+	(SELECT 
+		DATE(Date) AS Date, 
+		Close,
+		AVG(Close) OVER (PARTITION BY Symbol ORDER BY Date ROWS 50 PRECEDING) AS MA50,
+		AVG(Close) OVER (PARTITION BY Symbol ORDER BY Date ROWS 100 PRECEDING) AS MA100
+	FROM DJIA
+	WHERE Symbol = 'AAPL' AND (STRFTIME('%Y', Date) BETWEEN '2000' AND '2022')) AS i
+WHERE STRFTIME('%Y', i.Date) BETWEEN '2020' AND '2022'
+ORDER BY Date DESC;
+
+
+
+-- Counting the number of days that the close price is higher than close price of the date before 
+-- of AAPL stock in 2020
+SELECT COUNT(*)
+FROM
+	(SELECT 
+		Symbol, Date, Close,
+		ROUND((Close - previous_close), 2) AS 'Change',
+		ROUND(((Close - previous_close)*100/previous_close),2) AS '%Change',
+		CASE 
+			WHEN Close > previous_close THEN 'Up'
+			WHEN Close < previous_close THEN 'Down'
+		END AS 'Indicator'
+	FROM
+		(SELECT 
+			Symbol, DATE(date) AS Date, ROUND(Close, 2) AS Close, 
+			LAG (Close, 1, 0) OVER (PARTITION  BY Symbol ORDER BY Date) AS previous_close
+		FROM DJIA d 
+		WHERE STRFTIME('%Y', date) = '2022'
+		ORDER BY Symbol, date DESC))
+WHERE Indicator = 'Up'
+
+
+
+-- LOOKING AT MARKETS --
+
+-- Looking at the latest market indexes--
+SELECT 
+	Symbol, DATE(date) Date, ROUND(Close,2) Value, Volume
+FROM 
+	markets
+WHERE Date = (SELECT MAX(Date) FROM markets)
+GROUP BY Symbol 
+ORDER BY Date;
+
+
+-- Looking at the change of close price with close price of the date before
+SELECT 
+	Symbol, Date, Close, 
+	ROUND((Close - previous_close), 2) AS Gain,
+	ROUND(((Close - previous_close)*100/previous_close),2) AS '% Change',
+	Volume
+FROM
+	(SELECT 
+		Symbol, DATE(date) AS Date, ROUND(Close, 2) AS Close, Volume/1000 AS Volume,
+		LAG (Close, 1, 0) OVER (PARTITION  BY Symbol ORDER BY Date) AS previous_close
+	FROM markets m  
+	WHERE STRFTIME('%Y', date) BETWEEN '2020' AND '2022'
+	ORDER BY Symbol, date DESC)
+	
+
+-- Looking at the recent change of the market indexes
+SELECT 
+		Symbol, Date, Close, 
+		ROUND((Close - previous_close), 2) AS Gain,
+		ROUND(((Close - previous_close)*100/previous_close),2) AS '% Change',
+		Volume
+FROM
+		(SELECT 
+			Symbol, DATE(date) AS Date, ROUND(Close, 2) AS Close, Volume/1000 AS Volume,
+			LAG (Close, 1, 0) OVER (PARTITION  BY Symbol ORDER BY Date) AS previous_close
+		FROM markets m  
+		WHERE STRFTIME('%Y', date) BETWEEN '2020' AND '2022'
+		ORDER BY date DESC)
+LIMIT 3;
+
+
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Financial Analysis Projects
+
+### Motivation
+In my previous job, I joined in a project that we built a data analysis dashboard for individual investors. My tasks involved in designing dashboards that show stock market insights. With this project, I wanted to turn my previous work task into my personal end-to-end project. 
+
+### Contents:
+In this project, I would do:
+- Scraping the historical financial prices and fundamentals of Canadian S&P/TSX60 and Dow Jones Averages stocks, creating and importing data into SQLite database using Python.
+- Answering questions about financial analysis; Perform Extract, Transform and Load (ETL) data from data source for dashboard visualizing using SQL.
+- Creating measures and designed financial analysis dashboard; Performed data visualizing, descriptive and diagnostic data analytics using Power BI.
